452d4c7f821f819d4ad95b0bd4639ab64da541de
v_kailertb <kailert.b@gmail.com>
2020-03-10 18:03:36 +0700

    Update Function

--- SET-ConfigProfile/Form1.Designer.cs
+++ SET-ConfigProfile/Form1.Designer.cs
@@ -28,40 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblSource = new System.Windows.Forms.Label();
+            this.lblConfig = new System.Windows.Forms.Label();
+            this.txtSourcePath = new System.Windows.Forms.TextBox();
             this.btnBrowseSourceFolder = new System.Windows.Forms.Button();
+            this.txtConfigPath = new System.Windows.Forms.TextBox();
+            this.btnBrowseConfigFolder = new System.Windows.Forms.Button();
+            this.ddlProfile = new System.Windows.Forms.ComboBox();
+            this.lblProfile = new System.Windows.Forms.Label();
+            this.copyDataGridView = new System.Windows.Forms.DataGridView();
+            this.isCheckCopy = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.copyDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // lblSource
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(12, 15);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(73, 13);
+            this.lblSource.TabIndex = 0;
+            this.lblSource.Text = "Source Folder";
+            this.lblSource.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
-            // label2
+            // lblConfig
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(2, 73);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.lblConfig.AutoSize = true;
+            this.lblConfig.Location = new System.Drawing.Point(16, 41);
+            this.lblConfig.Name = "lblConfig";
+            this.lblConfig.Size = new System.Drawing.Size(69, 13);
+            this.lblConfig.TabIndex = 1;
+            this.lblConfig.Text = "Config Folder";
             // 
-            // textBox1
+            // txtSourcePath
             // 
-            this.textBox1.Location = new System.Drawing.Point(88, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(167, 20);
-            this.textBox1.TabIndex = 2;
+            this.txtSourcePath.Location = new System.Drawing.Point(91, 12);
+            this.txtSourcePath.Name = "txtSourcePath";
+            this.txtSourcePath.Size = new System.Drawing.Size(224, 20);
+            this.txtSourcePath.TabIndex = 2;
             // 
             // btnBrowseSourceFolder
             // 
-            this.btnBrowseSourceFolder.Location = new System.Drawing.Point(281, 10);
+            this.btnBrowseSourceFolder.Location = new System.Drawing.Point(321, 10);
             this.btnBrowseSourceFolder.Name = "btnBrowseSourceFolder";
             this.btnBrowseSourceFolder.Size = new System.Drawing.Size(75, 23);
             this.btnBrowseSourceFolder.TabIndex = 3;
@@ -69,17 +77,73 @@
             this.btnBrowseSourceFolder.UseVisualStyleBackColor = true;
             this.btnBrowseSourceFolder.Click += new System.EventHandler(this.btnBrowseSourceFolder_Click);
             // 
+            // txtConfigPath
+            // 
+            this.txtConfigPath.Location = new System.Drawing.Point(91, 38);
+            this.txtConfigPath.Name = "txtConfigPath";
+            this.txtConfigPath.Size = new System.Drawing.Size(224, 20);
+            this.txtConfigPath.TabIndex = 4;
+            // 
+            // btnBrowseConfigFolder
+            // 
+            this.btnBrowseConfigFolder.Location = new System.Drawing.Point(321, 36);
+            this.btnBrowseConfigFolder.Name = "btnBrowseConfigFolder";
+            this.btnBrowseConfigFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseConfigFolder.TabIndex = 5;
+            this.btnBrowseConfigFolder.Text = "Browse";
+            this.btnBrowseConfigFolder.UseVisualStyleBackColor = true;
+            this.btnBrowseConfigFolder.Click += new System.EventHandler(this.btnBrowseConfigFolder_Click);
+            // 
+            // ddlProfile
+            // 
+            this.ddlProfile.FormattingEnabled = true;
+            this.ddlProfile.Location = new System.Drawing.Point(91, 64);
+            this.ddlProfile.Name = "ddlProfile";
+            this.ddlProfile.Size = new System.Drawing.Size(224, 21);
+            this.ddlProfile.TabIndex = 7;
+            // 
+            // lblProfile
+            // 
+            this.lblProfile.AutoSize = true;
+            this.lblProfile.Location = new System.Drawing.Point(49, 67);
+            this.lblProfile.Name = "lblProfile";
+            this.lblProfile.Size = new System.Drawing.Size(36, 13);
+            this.lblProfile.TabIndex = 8;
+            this.lblProfile.Text = "Profile";
+            // 
+            // copyDataGridView
+            // 
+            this.copyDataGridView.AllowUserToDeleteRows = false;
+            this.copyDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.copyDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.isCheckCopy});
+            this.copyDataGridView.Location = new System.Drawing.Point(12, 91);
+            this.copyDataGridView.Name = "copyDataGridView";
+            this.copyDataGridView.Size = new System.Drawing.Size(384, 198);
+            this.copyDataGridView.TabIndex = 9;
+            // 
+            // isCheckCopy
+            // 
+            this.isCheckCopy.HeaderText = "";
+            this.isCheckCopy.Name = "isCheckCopy";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(514, 301);
+            this.ClientSize = new System.Drawing.Size(408, 301);
+            this.Controls.Add(this.copyDataGridView);
+            this.Controls.Add(this.lblProfile);
+            this.Controls.Add(this.ddlProfile);
+            this.Controls.Add(this.btnBrowseConfigFolder);
+            this.Controls.Add(this.txtConfigPath);
             this.Controls.Add(this.btnBrowseSourceFolder);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSourcePath);
+            this.Controls.Add(this.lblConfig);
+            this.Controls.Add(this.lblSource);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.copyDataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -87,10 +151,16 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblSource;
+        private System.Windows.Forms.Label lblConfig;
+        private System.Windows.Forms.TextBox txtSourcePath;
         private System.Windows.Forms.Button btnBrowseSourceFolder;
+        private System.Windows.Forms.TextBox txtConfigPath;
+        private System.Windows.Forms.Button btnBrowseConfigFolder;
+        private System.Windows.Forms.ComboBox ddlProfile;
+        private System.Windows.Forms.Label lblProfile;
+        private System.Windows.Forms.DataGridView copyDataGridView;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn isCheckCopy;
     }
 }
 
--- SET-ConfigProfile/Form1.cs
+++ SET-ConfigProfile/Form1.cs
@@ -8,29 +8,171 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace SET_ConfigProfile
 {
     public partial class Form1 : Form
     {
+        #region Constructor
+        public List<SourcePathList> sourcePathList { get; set; }
+        public List<ConfigFilePathList> configFilePathList { get; set; }
+
+        public class FilePathList
+        {
+            public int RowIndex { get; set; }
+            public string FileFullPathProfile { get; set; }
+            public string FileFullPath { get; set; }
+            public string FilePath { get; set; }
+            public string Filename { get; set; }
+            public string FileExtension { get; set; }
+            public string FileProfile { get; set; }
+            public string Status { get; set; }
+        }
+
+        public class ConfigFilePathList
+        {
+            public string FileFullPath { get; set; }
+            public string FileDirectoryPath { get; set; }
+
+            public string FilePath { get; set; }
+            public string Filename { get; set; }
+            public string FileExtension { get; set; }
+            public string FileProfile { get; set; }
+        }
+
+
+        public class SourcePathList
+        {
+            public string FullPath { get; set; }
+            public string DirectoryPath { get; set; }
+
+        }
+
+        public class ProfileList
+        {
+            public string Ext { get; set; }
+            public string DotExt { get; set; }
+
+        }
+
+        List<string> exceptList = new List<string>() { ".git", "packages", "bin", "obj" };
+        List<string> exceptExtList = new List<string>() { ".pfx"};
+
+        public class CopyList
+        {
+            public int RowIndex { get; set; }
+            public bool isCheckCopy { get; set; }
+            public string DirectoryPath { get; set; }
+            public string Filename { get; set; }
+            //public string FileFullPath { get; set; }
+            //public string FilePath { get; set; }
+            //public string FileExtension { get; set; }
+            //public string FileProfile { get; set; }
+        }
+        #endregion
+
+
+        /// <summary>
+        /// 
+        /// </summary>
         public Form1()
         {
             InitializeComponent();
+            ddlProfile.Enabled = false;
         }
 
         private void btnBrowseSourceFolder_Click(object sender, EventArgs e)
         {
-            using (var fbd = new OpenFileDialog())
+            sourcePathList = new List<SourcePathList>();
+            var applicationPath = Application.StartupPath;
+
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = applicationPath;
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
             {
-                DialogResult result = fbd.ShowDialog();
+                var folderPath = dialog.FileName;
+                txtSourcePath.Text = folderPath;
 
-                //if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
-                //{
-                //    string[] files = Directory.GetFiles(fbd.SelectedPath);
+                var allFolderList = System.IO.Directory.GetDirectories(folderPath, "*", System.IO.SearchOption.AllDirectories).ToList();
+                var allFolderListWithoutExcept = allFolderList.Distinct().ToList();
+                foreach (var item in exceptList)
+                {
+                    var exceptPath = Path.Combine(folderPath, item);
+                    allFolderListWithoutExcept = allFolderListWithoutExcept.Where(stringToCheck => !stringToCheck.Contains(exceptPath)).ToList();
+                }
 
-                //    System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
-                //}
+                foreach (var path in allFolderListWithoutExcept)
+                {
+                    var item = new SourcePathList();
+
+                    item.FullPath = path;
+                    item.DirectoryPath = path.Replace(folderPath, "");
+                    sourcePathList.Add(item);
+
+                }
+
             }
+
+        }
+
+        private void btnBrowseConfigFolder_Click(object sender, EventArgs e)
+        {
+            configFilePathList = new List<ConfigFilePathList>();
+
+            var applicationPath = Application.StartupPath;
+
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = applicationPath;
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                var folderPath = dialog.FileName;
+                txtConfigPath.Text = folderPath;
+
+                //TODO Get List FilePath List
+                var allfilesList = Directory.GetFiles(folderPath, "*.*", SearchOption.AllDirectories).ToList();
+                var allfilesListWithoutExcept = allfilesList;
+
+                foreach (var item in exceptList)
+                {
+                    var exceptPath = Path.Combine(folderPath, item);
+                    allfilesListWithoutExcept = allfilesListWithoutExcept.Where(stringToCheck => !stringToCheck.Contains(exceptPath)).ToList();
+                }
+
+                foreach (var file in allfilesListWithoutExcept)
+                {
+                    FileInfo fileInfo = new FileInfo(file);
+                    var extensionFile = fileInfo.Extension;
+
+                    var checkIsFile = fileInfo.FullName.Replace(extensionFile, "");
+                    FileInfo checkIsFileInfo = new FileInfo(checkIsFile);
+                    var checkIsFileInfoExt = checkIsFileInfo.Extension;
+
+                    bool isCheckIsFileInfoExtNotInExceptList = exceptExtList.Where(x => x == checkIsFileInfoExt).ToList().Count() == 0;
+                    if (isCheckIsFileInfoExtNotInExceptList)
+                    {
+                        var item = new ConfigFilePathList();
+                        item.FileFullPath = fileInfo.FullName;
+                        item.FileDirectoryPath = fileInfo.FullName.Replace(folderPath, "").Replace(extensionFile, "");
+                        item.Filename = "";
+                        item.FileExtension = extensionFile;
+                        configFilePathList.Add(item);
+                    }
+
+                }
+
+                var ddlProfileList = configFilePathList.Select(x => new ProfileList { Ext = x.FileExtension.Replace(".", ""), DotExt = x.FileExtension }).GroupBy(x => new { x.Ext, x.DotExt }).Select(n => new { n.Key.Ext, n.Key.DotExt }).ToList();
+                ddlProfile.DataSource = ddlProfileList;
+                ddlProfile.DisplayMember="Ext";
+                ddlProfile.ValueMember="DotExt";
+                ddlProfile.Enabled = true;
+
+
+
+            }
+
         }
     }
 }
